f5e067eb4b29daa24753b2f98741cdcde4ca3c2e
Oscar Villalta <bagglerock@gmail.com>
2018-03-27 13:20:47 -0400

    added the photos page for now

--- assets/js/kf.js
+++ assets/js/kf.js
@@ -1,28 +1,3 @@
-//slideshow -
-
-//what is happening
-
-//all of the pictures
-var picturesArray = ["../images/slideshow/IMG_0400.PNG", ];
-
-var showImage;
-
-//interval to change pic
-var nSeconds = 3000;
-var count;
-
-function startSlideShow () {
-    showImage = setInteval(function() {
-        nextImage();
-    }, nSeconds);
-}
-
-function stopSlideShow () {
-    clearImage(showImage);
-}
-
-function changeMainPic (pic) {}
-
-function changeHighlightedPic (pic) {}
-
-displayImage();
+$(document).ready(function(){
+  $("#quote").fadeIn(1000);
+});
