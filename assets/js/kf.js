e29dc616051b07204fd7c9f0f8b0a7b1ce8e8181
Oscar Villalta <bagglerock@gmail.com>
2018-04-12 15:22:35 -0400

    changing some of the text around

--- assets/js/kf.js
+++ assets/js/kf.js
@@ -1,5 +1,3 @@
-var accessToken = "EAACEdEose0cBACOTQxwO8ZAcqCbzOYazGTNohQzBuSmFH5GJzMUZBp5rbhqRSrwZBzNjJ6LwM8aVIb6JJw0Omt2GpximIKLNUFK6ONN5KkvZBoHCybjvytW1lIZCwZC3gVvGqLM01FJq6qtITZAQrax9l2XZCco0ISc4L5nfZA01rBk9lxMC3olmXCMcezLE2UL6sm2WmK9zlLwZDZD";
-
 $(document).ready(function() {
   
   $("#quote").fadeIn(1000);
