942a722ed37808401825b82bee2c6ab20d8255e0
bagglerock <bagglerock@gmail.com>
2021-05-23 22:54:42 -0400

    feat: add script to read contents of image directory to load all images to photos

--- assets/js/kf.js
+++ assets/js/kf.js
@@ -3,19 +3,10 @@ $(document).ready(function() {
   $("#quote").fadeIn(1500);
 
   $(".section").hover(function(){
-    //console.log("hover works");
     $("img").fadeIn();
   })
-
-
-  $(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(window).scrollTop() > 1000){
-            //console.log("something");
-        }
-      });
-    });
 });
+
 
 
 
